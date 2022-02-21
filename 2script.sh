cbeb6b06bfd888743a86d2fdf671b6448df7feb4
root <root@debian10.linuxvmimages.local>
2022-02-21 17:26:07 -0600

    correção do codigo do php

--- 2script.sh
+++ 2script.sh
@@ -12,12 +12,16 @@ netstat -tuplpn | grep :80 ; read -t 5 -p "";
 clear ; read -p "QUARTO PASSO: VERIFICAR O ENDEREÇO DO SERVIDOR NO BROWSER           " z;
 ifconfig enp0s3 | grep 'inet' ; read -p "digite o ipv4 encontrado: " p;
 elinks $p"/index.html";
+  
+echo '<h1>Fui buscar cobre... E encontrei ouro !!!</h1>' > /var/www/html/index.html ;
+
+read -p "Qualquer alteração no html reflete no navegador";
 
 clear ; read -p "QUINTO PASSO: INSTALAR O PHP 5 E DEPENDÊNCIAS           " z;
-aptitude install php5 libapache2-mod-php5;
+aptitude install php ;
 
 clear ; read -p "SEXTO PASSO: ACRESCENTAR O ARQUIVO INFO.PHP PARA O SERVIDOR   " z;
-echo '<?php  phpinfo():  ?>' > /var/www/info.php;
+echo '<?php  phpinfo();  ?>' > /var/www/html/info.php;
 
 clear ; read -p "ÚLTIMO PASSO: VERIFICAR O ARQUIVO PHP COM O BROWSER           " z;
 elinks $p"/info.php"; clear;
