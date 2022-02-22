88d1532b0fd8a3849481d77fdb556e693b51ab6d
root <root@debian10.linuxvmimages.local>
2022-02-22 17:22:40 -0600

    mandei mais um

--- 3script.sh
+++ 3script.sh
@@ -2,7 +2,7 @@
 
 clear ; read  -t 5  -p "PASSO 1: INSTALAR O BIND ------> apt install bind9                        " ;
 
-apt install bind9 -y;                       
+apt install bind9 ;                       
 
 clear ; read  -t 5  -p "PASSO 2: INCLUIR AS ZONAS DIRETAS E REVERSAS NO ARQUIVO named.conf                " ;
 
@@ -18,25 +18,25 @@ cat db10edilton.txt >> /etc/bind/db.10
 
 clear ; read  -t 5  -p "PASSO 5:  CONFIGURAR O ARQUIVO resolv.conf    " ;
 
+echo "nameserver 192.168.1.15" > /etc/resolv.conf
 echo "domain edilton.gov.br" >> /etc/resolv.conf ; 
 echo "search edilton.gov.br" >> /etc/resolv.conf ; 
-echo "192.168.1.15" >> /etc/resolv.conf ; 
 
 clear ; read  -t 5  -p "PASSO 6: VERIFICA O STATUS DO BIND   " ;
 
-/etc/init.d/bind9 status;
+/etc/init.d/bind9 status; read -p "";
 
 clear ; read  -t 5  -p "PASSO 7: REINICIAR O BIND COM AS ALTERACOES " ;
 
-/etc/init.d/bind9 restart;
+/etc/init.d/bind9 restart; read -p "";
 
 clear ; read  -t 5  -p "PASSO 8: CHECAR AS CONFIGURACOES COM NSLOOKUP " ;
 
-nslookup www.edilton.gov.br;
+nslookup www.edilton.gov.br;   read -p "" ;
 
 clear ; read  -t 5  -p "PASSO 9 REALIZAR O PING PARA O DOMINIO CRIADO:  " ;
 
-ping www.edilton.gov.br;
+ping www.edilton.gov.br;  
 
 
 clear ; read  -t 5  -p "MISSAO CUMPRIDA !!!" ;
