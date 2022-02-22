25907a6def138e9829a3285488dd62697c65fd5e
Edilton <ediltonx@hotmail.com>
2022-02-22 15:50:20 -0300

    corrigindo trabalho 3

--- 3script.sh
+++ 3script.sh
@@ -10,16 +10,16 @@ cp /etc/bind/named.conf /etc/bind/named.conf.bkp ; cat namedconf.txt >> /etc/bin
 
 clear ; read  -t 5  -p "PASSO 3: CRIAR O ARQUIVO DE ZONA DIRETA " ;
 
-cat dbatv3.txt >> /etc/bind/db.atv3.gov.br
+cat dbedilton.txt >> /etc/bind/db.edilton.gov.br
 
 clear ; read  -t 5  -p "PASSO 4:  CRIAR O ARQUIVO DE ZONA REVERSA        " ;
 
-cat db10atv3.txt >> /etc/bind/db.10
+cat db10edilton.txt >> /etc/bind/db.10
 
 clear ; read  -t 5  -p "PASSO 5:  CONFIGURAR O ARQUIVO resolv.conf    " ;
 
-echo "domain atv3.gov.br" >> /etc/resolv.conf ; 
-echo "search atv3.gov.br" >> /etc/resolv.conf ; 
+echo "domain edilton.gov.br" >> /etc/resolv.conf ; 
+echo "search edilton.gov.br" >> /etc/resolv.conf ; 
 echo "192.168.1.15" >> /etc/resolv.conf ; 
 
 clear ; read  -t 5  -p "PASSO 6: VERIFICA O STATUS DO BIND   " ;
@@ -32,11 +32,11 @@ clear ; read  -t 5  -p "PASSO 7: REINICIAR O BIND COM AS ALTERACOES " ;
 
 clear ; read  -t 5  -p "PASSO 8: CHECAR AS CONFIGURACOES COM NSLOOKUP " ;
 
-nslookup www.atv3.gov.br;
+nslookup www.edilton.gov.br;
 
 clear ; read  -t 5  -p "PASSO 9 REALIZAR O PING PARA O DOMINIO CRIADO:  " ;
 
-ping www.atv3.gov.br;
+ping www.edilton.gov.br;
 
 
 clear ; read  -t 5  -p "MISSAO CUMPRIDA !!!" ;
