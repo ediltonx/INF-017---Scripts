92d0c705d4d70651920936246fe5e6f9bfdb3125
Edilton <ediltonx@hotmail.com>
2022-02-24 20:52:50 -0300

    ajuste final no quarto script

--- 4script.sh
+++ 4script.sh
@@ -1,11 +1,14 @@
+
+ifconfig enp0s3 | grep 'inet' ; read -p "Antes de comecar, digite o ipv4 encontrado: " ipv4;
+
 clear ; read  -t 5  -p "PASSO 1: CRIAR DIRETORIOS EM /var/www                       " ;
 
 mkdir /var/www/edilton.gov.br ; mkdir /var/www/edilton.org ;
 
 read  -t 1  -p "PASSO 2: ADICIONAR UM ARQUIVO INDEX.HTML NOS DIRETORIOS CRIADOS              " ;
 
-echo "<h1>Site de Edilton ponto GOV ponto BR</h1>" > /var/www/edilton.gov.br/index.html ;
-echo "<h1>Site de Edilton dot ORG </h1>" > /var/www/edilton.org/index.html ;
+echo "<html><h1> Site de Edilton ponto GOV ponto BR </h1></html>" > /var/www/html/edilton.gov.br/index.html ;
+echo "<html><h1> Site de Edilton dot ORG </h1></html>" > /var/www/html/edilton.org/index.html ;
 
 read  -t 5  -p "";
 
@@ -16,18 +19,19 @@ echo "
 #
 #
 <VirtualHost *>
+
         ServerAdmin webmaster@www.edilton.gov.br
         ServerName  www.edilton.gov.br
         ServerAlias edilton.gov.br
 
         # Indexes + Directory Root.
         DirectoryIndex index.html
-        DocumentRoot /home/www/edilton.gov.br
+        DocumentRoot /home/www/html/edilton.gov.br
 
 </VirtualHost>
 
 
-" > /etc/apache2/sites-available/www.edilton.gov.br ;
+" > /etc/apache2/sites-available/www.edilton.gov.br.conf ;
 
 
 echo "
@@ -35,38 +39,47 @@ echo "
 #
 #
 <VirtualHost *>
+
         ServerAdmin webmaster@www.edilton.org
         ServerName  www.edilton.org
         ServerAlias www.edilton.org
 
         # Indexes + Directory Root.
         DirectoryIndex index.html
-        DocumentRoot /home/www/www.edilton.org
+        DocumentRoot /home/www/html/www.edilton.org
 
 </VirtualHost>
 
 
-" > /etc/apache2/sites-available/www.edilton.org ;
+" > /etc/apache2/sites-available/www.edilton.org.conf ;
 
 
+ls /etc/apache2/sites-available/*.conf;
+
+read  -t 3  -p "os arquivos .conf das paginas foram configurados nos respectivos diretorios";
 
 
-read  -t 3  -p "os arquivos foram configurados nos respectivos diretorios";
+clear ; read  -t 5  -p "PASSO 4: HABILITAR OS SITES E VERIFICAR SE FORAM HABILITADOS    " ;
 
+/etc/apache2/sites-available/a2ensite www.edilton.org ; 
+/etc/apache2/sites-available/a2ensite www.edilton.gov.br ;
 
-clear ; read  -t 5  -p "PASSO 4: HABILITAR OS SITES          " ;
+ls /etc/apache2/sites-enabled/ ;
 
-a2ensite www.edilton.org ; a2ensite www.edilton.gov.br ;
+read  -t 5  -p "PASSO 5: REATIVAR O SERVIDOR DO APACHE   " ;
 
+/etc/init.d/apache2 reload ; 
+system-ctl reload apache2 ;
+service apache2 stop;
+service apache2 start;
 
-read  -t 5  -p "PASSO 5:      " ;
+read  -t 5  -p "PASSO 6: CHECAR OS SITES NO NAVEGADOR   " ;
 
-/etc/init.d/apache2 reload ;
+elinks $ipv4/www.edilton.gov.br;
 
+elinks $ipv4/www.edilton.org.br;
 
-
-clear ; read  -t 5  -p " MISSAO CUMPRIDA !!!  " ;
-
+clear; read   -p " MISSAO CUMPRIDA !!!  " ;
 
 clear ; read  -t 5  -p "FIM" ;
 
