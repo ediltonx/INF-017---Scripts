508ec4d39e93cdf1c8e1723ab1960c68f8a9cd99
Edilton <ediltonx@hotmail.com>
2022-02-27 19:26:25 -0300

    trabalho 9 feito?!

--- 9script.sh
+++ 9script.sh
@@ -1,32 +1,48 @@
-clear ; read  -t 5  -p "PASSO 1:                        " ;
+#!/bin/bash
 
+clear ; read  -t 5  -p "PASSO 1: INSTALAR O SQUID      " ;
 
-clear ; read  -t 5  -p "PASSO 2:               " ;
+apt install squid;
 
- 
+clear ; read  -t 5  -p "PASSO 2: FAZER BACKUP DO ARQUIVO DE CONFIGURACAO ORIGINAL DO SQUID              " ;
 
-clear ; read  -t 5  -p "PASSO 3:  " ;
+nano /etc/squid/squid.conf; 
 
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bckp;
 
-clear ; read  -t 5  -p "PASSO 4:          " ;
+clear ; read  -t 5  -p "PASSO 3: EXPORTAR O MEU ARQUIVO DE CONFIGURACAO DO SQUID  " ;
 
+cat confsquid.txt | less ; 
 
-clear ; read  -t 5  -p "PASSO 5:      " ;
+cat confsquid.txt > /etc/squid/squid.conf;
 
+clear ; read  -t 5  -p "PASSO 4: CADASTRAR SITES PARA SEREM BLOQUEADOS EM /etc/squid/bloqueados.txt          " ;
 
-clear ; read  -t 5  -p "PASSO 6:  " ;
+nano /etc/squid/bloqueados.txt; 
 
+clear ; read  -t 5  -p "PASSO 5: CRIAR PASTAS PARA OS ARQUIVOS DE CACHE E LOG     " ;
 
-clear ; read  -t 5  -p "PASSO 7:  " ;
+mkdir /var/cache/squid; touch /var/log/squid/store.log;
 
+clear ; read  -t 5  -p "PASSO 6: DEFINIR PERMISSAO DO USUARIO PARA AS PASTAS DO SQUID  " ;
 
-clear ; read  -t 5  -p "PASSO 8:  " ;
+chown -R proxy.proxy /var/log/squid;
+chown -R proxy.proxy /var/cache/squid;
 
+clear ; read  -t 5  -p "PASSO 7: CHECAR AS CONFIGURACOES REALIZADAS COM O COMANDO squid -z " ;
 
-clear ; read  -t 5  -p "PASSO 9:  " ;
+squid -z;
 
+read  -t 5  -p "PASSO 8: LIMPAR AS REGRAS DE FIREWALL DO IPTABLES " ;
 
-clear ; read  -t 5  -p "PASSO 10:  " ;
+iptables -t nat -F;
+echo 0 > /proc/sys/net/ipv4/ip_forward;
+
+clear ; read  -t 5  -p "PASSO 9: REINICIAR SQUID " ;
+
+/etc/init.d/squid restart;
+
+clear ; read  -t 5  -p "PASSO 10: CHECAR O PROXY NO NAVEGADOR " ;
 
 
 clear;
