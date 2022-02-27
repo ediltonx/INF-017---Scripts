ef9347f2d1a82edc5d59d2865689adc2d8e89616
root-INF017 <root-inf017@edilton.gov.br>
2022-02-27 07:22:47 -0600

    10 script feito

--- 10script.sh
+++ 10script.sh
@@ -1,32 +1,21 @@
-clear ; read  -t 5  -p "PASSO 1:                        " ;
+#!/bin/bash
+
+clear ; read  -t 5  -p "PASSO 1: INSTALAR O OPENSSH SERVER        " ;
+
+apt install openssh-server ; 
+
+clear ; read  -t 5  -p "PASSO 2: CONFIGURAR PARÂMETROS DO HOST     " ;
+
+nano  /etc/ssh/sshd_config;
+nano /etc/ssh/ssh_config ; 
+
+clear ; read  -t 5  -p "PASSO 3: ATIVAR O SERVIDOR SSH NA MÁQUINA DESEJADA  " ;
+
+service ssh start ; service ssh restart ;
+
+clear ; read  -t 5  -p "PASSO 4: CONFIGURAR PARÂMETROS NO CLIENTE E AGUARDAR CONEXÃO NO IP ABAIXO    " ;
+ifconfig | grep "inet";
 
 
-clear ; read  -t 5  -p "PASSO 2:               " ;
-
- 
-
-clear ; read  -t 5  -p "PASSO 3:  " ;
 
 
-clear ; read  -t 5  -p "PASSO 4:          " ;
-
-
-clear ; read  -t 5  -p "PASSO 5:      " ;
-
-
-clear ; read  -t 5  -p "PASSO 6:  " ;
-
-
-clear ; read  -t 5  -p "PASSO 7:  " ;
-
-
-clear ; read  -t 5  -p "PASSO 8:  " ;
-
-
-clear ; read  -t 5  -p "PASSO 9:  " ;
-
-
-clear ; read  -t 5  -p "PASSO 10:  " ;
-
-
-clear;
